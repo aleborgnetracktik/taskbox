ab9c9d78be44ccb91ba3387ad93a6e214467c94c
aleborgnetracktik <alexandre.leborgne@tracktik.com>
2021-01-15 14:00:05 -0500

    add InboxScreen container

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <InboxScreen />
   </div>
 </template>
 
 <script>
-import store from "@/store";
-import TaskList from "@/components/PureTaskList";
+import store from "./store";
+import InboxScreen from "@/components/InboxScreen";
 
 export default {
   name: "App",
   store,
   components: {
-    TaskList
+    InboxScreen,
   }
 };
 </script>
